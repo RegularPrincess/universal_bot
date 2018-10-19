fae3084c3829dc86c98c7bf31de676c8b6fdb0e0
JMFedorov <jmfedorov@gmail.com>
2018-10-19 13:49:44 +0300

    new question

--- service.py
+++ service.py
@@ -284,10 +284,10 @@ def start_conwersation(number):
 
 
 IN_ADMIN_PANEL['259056624'] = 0
-message_processing('259056624', 'whatsapp 79991577222', cnst.VK)
-message_processing('79991577222', '3r56g', cnst.WHATSAPP)
-message_processing('79991577222', '222222', cnst.WHATSAPP)
-message_processing('79991577222', '2', cnst.WHATSAPP)
+# message_processing('259056624', 'whatsapp 79991577222', cnst.VK)
+# message_processing('79991577222', '3r56g', cnst.WHATSAPP)
+# message_processing('79991577222', '222222', cnst.WHATSAPP)
+# message_processing('79991577222', '2', cnst.WHATSAPP)
 
 
 
